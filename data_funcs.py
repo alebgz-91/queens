6d24383cce05ca0584e81b64efc34091c9b5c708
Alessandro <alessandro.bigazzi.91@gmail.com>
2025-06-29 21:45:09 +0100

    Abstracting templates path

--- data_funcs.py
+++ data_funcs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import read_excel
 
 from utils import *
 
@@ -47,14 +48,55 @@ def transform_dukes_1_3(url: str, sheet_suffixes: list):
 
 
 
+def transform_dukes_1_1_1(url: str, sheet_list: list):
+    """
+    Cleans the sheets in table 1.1.1 and transform to flat format
+    Args:
+        url: full HTML address of table 1.1.1
+        sheet_list: list of sheets to parse
+
+    Returns:
+        a dictionary containing the transformed tables and
+
+    """
+
+    out = {}
+
+    for table in table_list:
+        table = read_sheet_with_titles(utl, sheet_name="1.1.1." + sheet)
+
+        # remove col1 and rename index
+        table.drop(columns=["Column1"], inplace = True)
+        table.index.name = "row"
+
+        # load template
+        template = pd.read_excel(TEMPLATE_PATH_CH_1,
+                                 sheet_name = "1.1.1." + sheet)
+
+        res = pd.merge(table,
+                       template,
+                       on="row")
+
+
+        # flatten columns
+        res = pd.melt(out,
+                      id_vars = template.columns,
+                      var_name="Year",
+                      value_name="Value")
+
+        out.update({f"1_1_1_"})
+
+    return {"1.1.1." + sheet: out}
+
+
 dukes_tables_ch_1 = get_dukes_urls(
     "https://www.gov.uk/government/statistics/energy-chapter-1-digest-of-united-kingdom-energy-statistics-dukes")
 
 url = dukes_tables_ch_1["dukes_1_1_1"]["url"]
 
-def transform_dukes_1_1_1(url: str):
+def transform_dukes_1_1_2(url: str):
     """
-    Clean DUKES 1.3 and transform to flat format.
+    Clean DUKES 1.1.2 and transform to flat format.
 
     Args:
         url: full HTTP address of Excel table
@@ -64,18 +106,18 @@ def transform_dukes_1_1_1(url: str):
 
     """
     # read and transpose
-    table = read_sheet_with_titles(url, sheet_name="1.1.1").T
+    table = read_sheet_with_titles(url, sheet_name="1.1.2").T
 
     # remove raw columns labels
     table.drop(columns=[table.columns[0]], inplace=True)
 
     template = pd.read_excel(TEMPLATE_PATH_CH_1,
-                             sheet_name="1.1.1")
+                             sheet_name="1.1.2").T
 
     table = pd.merge(table,
                      template,
-                     left_on = "row",
-                     right_index=True)
+                     right_on = "row",
+                     left_index=True)
 
     # to flat format
     table = pd.melt(table,
