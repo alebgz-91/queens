80d66ce0722add79f3c5e058d726548ef198bf1c
Alessandro <alessandro.bigazzi.91@gmail.com>
2025-07-10 22:10:32 +0100

    Consolidating helper functions

--- data_funcs.py
+++ data_funcs.py
@@ -1,89 +1,89 @@
 import pandas as pd
+
+from IGNORE_dev_notebook import table_no
 from utils import *
 
 TEMPLATE_PATH_CH_1 = "data/templates/dukes_ch_1.xlsx"
 
 
-def transform_dukes_1_3(url: str, sheet_suffixes: list):
+def process_sheet_to_frame(
+        url: str,
+        data_collection: str,
+        table_name: str = None,
+        sheet_names: list,
+        var_to_melt: str = "Year",
+        extra_id_vars: list = [],
+        map_on_cols: bool = False):
     """
-    Clean DUKES 1.3 and transform to machine readable format.
+    A chapter-agnostic function that processes individual sheets into separate frames.
+    The list of sheet provided will be parsed and each worksheet will be returned as an individual
+    processed dataframe.
+    The function handles tables with time index on either axes, allowing the re-mapping of column
+    headings through a template if needed.
+
     Args:
-        url: direct URL to Excel workbook
-        sheet_suffixes: a list of suffixes corresponding to sheet names, i.e. 1.3.A and 1.3.B
+        url: the full HTML path of the workbook
+        data_collection: name of the series the workbook belongs to (i.e. "dukes")
+        table_name: optional, full name of the table (i.e. 1.2). If sheet_names only contains one sheet and table_name is None, it will be inferred
+        sheet_names: list of sheets to be processed
+        var_to_melt: if map_on_cols is False, this is the name of the variable on the columns, otherwise is the name of the index column. Default is "Year"
+        extra_id_vars: additional columns to be used as id_vars. Not usable if map_on_cols is Trye,
+        map_on_cols: whether to transpopse the table before mapping to the template. Default is False.
 
     Returns:
-        a dictionary of transformed dataframes, one for each sheet passed in sheet_suffixes
 
     """
+
+    # if wishing to use mapping on cols then cannot allow exta id_vars
+    if map_on_cols and (len(extra_id_vars) != 0):
+        raise ValueError("Cannot include extra id vars while transposing: use the mapping template instead.")
 
     out = {}
 
-    for sheet in sheet_suffixes:
-        # get table from GOV.UK
-        table = read_sheet_with_titles(file_path=url,
-                                       sheet_name=f"1.3.{sheet}")
+    # if only one sheet is provided and no table_name is passed
+    # then infer table_name from sheet
+    if (table_name is None) and (len(sheet_names) == 1):
+        table_name = sheet_names[0]
 
-        # drop first column as it contains raw labels
-        table.drop(columns=table.columns[0], inplace=True)
+    for sheet in sheet_names:
+
+        # get table from GOV.UK
+        if map_on_cols:
+            table = (read_sheet_with_titles(file_path = url,
+                                            sheet_name = sheet_names)
+                     .set_index(var_to_melt)
+                     .T
+                     .reset_index(drop=False))
+        else:
+            table = read_sheet_with_titles(file_path = url,
+                                           sheet_name = sheet)
+
+        # first columns is dropped unless otherwise specified
+        if table.columns[0] not in extra_id_vars:
+            table.drop(columns = table.columns[0],
+                       inplace=True)
 
         # get corresponding template
-        template = pd.read_excel(io=TEMPLATE_PATH_CH_1,
-                                 sheet_name=f"1.3.{sheet}")
+        template = read_sheet_with_titles(file_path = TEMPLATE_PATH_CH_1,
+                                          sheet_name = sheet_names)
 
         # join with template
         table = pd.merge(table,
                          template,
-                         right_on="row",
-                         left_index=True)
+                         right_on = "row",
+                         left_index = True)
 
         table = pd.melt(table,
-                        id_vars=template.columns,
-                        var_name="Year",
-                        value_name="Value")
+                        id_vars = template.columns + extra_id_vars,
+                        var_name = var_to_melt,
+                        value_name = "Value")
 
-        out.update({f"dukes_1_1_{sheet}": table})
-
-    return out
-
-
-def transform_dukes_1_1_1(url: str, sheet_list: list):
-    """
-    Cleans the sheets in table 1.1.1 and transform to flat format
-    Args:
-        url: full HTML address of table 1.1.1
-        sheet_list: list of sheets to parse
-
-    Returns:
-        a dictionary containing the transformed tables and
-
-    """
-
-    out = {}
-
-    for table in table_list:
-        table = read_sheet_with_titles(utl, sheet_name="1.1.1." + sheet)
-
-        # remove col1 and rename index
-        table.drop(columns=["Column1"], inplace=True)
-        table.index.name = "row"
-
-        # load template
-        template = pd.read_excel(TEMPLATE_PATH_CH_1,
-                                 sheet_name="1.1.1." + sheet)
-
-        res = pd.merge(table,
-                       template,
-                       on="row")
-
-        # flatten columns
-        res = pd.melt(res,
-                      id_vars=template.columns,
-                      var_name="Year",
-                      value_name="Value")
-
-        out.update({"dukes_1_1_1": res})
+        output_name = data_collection + "_" + sheet_names.replace(".", "_")
+        out.update({output_name: table})
 
     return out
+
+
 
 
 def transform_dukes_1_1_x(url: str, table_id: int):
@@ -170,30 +170,29 @@ def transform_dukes_1_1_5(url: str):
     return {"dukes_1_1_5": res}
 
 
-table_no = "1.2"
 
 
-def transform_balance_multisheet(url: str, table_no: str):
+def process_multi_sheets_to_frame(
+        url: str,
+        table_name: str):
     """
-    A chapter-agnostic function for reading balance workbooks
-    where years are reported on separate sheets and fuels are reported on columns
-
-    There are special behaviours for tables 1.1 and 6.1, which
-    have slight format changes across the sheets.
+    A chapter-agnostic function for processing multisheet workbooks
+    where each year is reported on a separate sheet, while columns on each sheet need to be melted.
+    The function has special conditional behaviour for some tables that require extra manipulation.
 
     Args:
         url: the full HTTP address of the table
-        table_no: the DUKES table number (x.y.z)
+        table_name: the DUKES table number (x.y.z)
 
     Returns:
-        a dictionary containing the transformed sheet as a single dataframe
+        a dictionary containing the transformed sheets as a single dataframe
     """
     # read the whole workbook
     wb = read_sheet_with_titles(url, sheet_name=None)
 
     # read the template
     template = read_sheet_with_titles(TEMPLATE_PATH_CH_1,
-                                      sheet_name=table_no)
+                                      sheet_name=table_name)
 
     res = pd.DataFrame()
 
@@ -225,7 +224,7 @@ def transform_balance_multisheet(url: str, table_no: str):
         res = pd.concat([res, tab], axis=0)
 
     # clean former column heading
-    if table_no == "J.1":
+    if table_name == "J.1":
         # for heat reallocation, units need to be inferred
         res["unit"] = (res["fuel"]
                        .apply(lambda x: x.split("[")[-1])
@@ -234,5 +233,5 @@ def transform_balance_multisheet(url: str, table_no: str):
 
     res["fuel"] = res["fuel"].apply(lambda x: x.split("[")[0].strip())
 
-    table_name = table_no.replace(".", "_")
+    table_name = table_name.replace(".", "_")
     return {"dukes_" + table_name: res}
