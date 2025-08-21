e19a8141b1c6ba4c49161e8deaa5b4de512e1afa
Alessandro <alessandro.bigazzi.91@gmail.com>
2025-08-21 14:13:44 +0100

    __init__.py created

--- queens/__init__.py
+++ queens/__init__.py
@@ -3,8 +3,15 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 # configuration
-from queens.settings import set_config
-from queens.settings import setup_logging
-from queens.etl.bootstrap import initialize
-from queens.etl.process import ingest_tables, ingest_all_tables
+from queens.settings import set_config, setup_logging
+from queens.facade import  ingest, stage, info, metadata, versions, query, export
 
+__all__ = ["set_config",
+           "setup_logging",
+           "ingest",
+           "stage",
+           "query",
+           "metadata",
+           "info",
+           "versions",
+           "export"]
